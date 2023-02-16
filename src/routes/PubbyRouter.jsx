ed6a17e64342c7adf8d5136cf82f4dcde963e687
Calin Peters <calinpeters98@hotmail.nl>
2023-02-16 14:14:02 +0100

    Chore: create home view

--- src/routes/PubbyRouter.jsx
+++ src/routes/PubbyRouter.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import AllEvents from '../views/events/EventPage';
+import AllEvents from '../views/events/AllEventsView';
 export default function Router() {
     return (
         <BrowserRouter>
